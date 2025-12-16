58d6f2ca078f70640ff9600c77d0b03b4ba86a50
illya <ilaa64536@gmail.com>
2025-12-16 15:24:32 +0200

    Update serializers.py

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,3 +1,4 @@
+from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
@@ -18,13 +19,39 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj) -> str | None:
         request = self.context.get("request")
+        if request is None:
+            return obj.image.url
         return request.build_absolute_uri(obj.image.url) if obj.image else None
 
-    def get_ingredients(self, obj) -> list[str]:
-        return [str(pi.ingredient.name) for pi in obj.product_ingredient.all()]
+    @extend_schema_field(OpenApiTypes.STR)
+    def get_ingredients(self, obj):
+        if self.context.get("minimal"):
+            return [pi.ingredient.name for pi in obj.product_ingredient.all()]
 
-    def get_dough_types(self, obj) -> list[str]:
-        return [str(dt.name) for dt in obj.dough_types.all()]
+        ingredient_serializer = IngredientSerializer(
+            [pi.ingredient for pi in obj.product_ingredient.all()],
+            many=True,
+            context={
+                **self.context,
+                "minimal": True,
+            },
+        )
+        return ingredient_serializer.data
+
+    @extend_schema_field(OpenApiTypes.STR)
+    def get_dough_types(self, obj):
+        if self.context.get("minimal"):
+            return [str(dt.name) for dt in obj.dough_types.all()]
+
+        dough_type_serializer = DoughTypeSerializer(
+            obj.dough_types.all(),
+            many=True,
+            context={
+                **self.context,
+                "minimal": True,
+            },
+        )
+        return dough_type_serializer.data
 
     class Meta:
         model = Product
@@ -33,15 +60,30 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True, read_only=True)
+    products = serializers.SerializerMethodField()
     product_sizes = serializers.SerializerMethodField()
-    # product_sizes = ProductSizeSerializer(many=True, read_only=True)
+
+    @extend_schema_field(ProductSerializer(many=True))
+    def get_products(self, obj):
+        serializer = ProductSerializer(
+            obj.products.all(),
+            many=True,
+            context={
+                **self.context,
+                "minimal": True,
+            },
+        )
+        return serializer.data
 
     @extend_schema_field(ProductSizeSerializer(many=True))
     def get_product_sizes(self, obj):
         serializer = ProductSizeSerializer(
             [cp.product_size for cp in obj.category_product_size.all()],
             many=True,
+            context={
+                **self.context,
+                "minimal": True,
+            },
         )
         return serializer.data
 
